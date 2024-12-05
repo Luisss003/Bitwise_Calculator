30f680e0859b250586696175b4b1d19f7b0bb38f
Luis <lsaenz29484@gmail.com>
2024-12-04 23:38:31 -0600

    Added some code

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[]){
+
+    if(argc < 2){
+        printf("ERROR: Usage: bitwise_calcualtor [operation] [operand1]
+        [operand2] ..."     
+    }
+    printf("%d", argc);
+
+
+}
